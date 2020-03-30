370937b72b42d218a6647cbf010c3cc41f96dcc2
bruno <cariverplate77@gmail.com>
2020-03-30 16:52:56 -0300

    Agregado todo lo del pizarron de cervezas

--- resources/views/admin/beers/index.blade.php
+++ resources/views/admin/beers/index.blade.php
@@ -11,53 +11,51 @@
 		</div>
 	</div>
 
-	<div class="card">
-		<div class="card-body bg-light">
-			<table class="table table-striped table-hover">
-				<thead>
-					<th>Nombre</th>
-					<th>Estilo</th>
-					<th>Descripcion</th>
-					<th>Productor</th>
-					<th>Color</th>
-					<th>IBU</th>
-					<th>Alcohol</th>
-					<th>Precio</th>
-					<th>Precio HH</th>
-					<th>Editar</th>
-					<th>Eliminar</th>
-				</thead>
-				<tbody>
-					@foreach($beers as $beer)
-					<tr>
-						<td>{{ $beer->name }}</td>
-						<td>{{ $beer->style }}</td>
-						<td>{{ $beer->description }}</td>
-						<td>{{ $beer->brand }}</td>
-						<td>{{ $beer->color }}</td>
-						<td>{{ $beer->ibu }}</td>
-						<td>{{ $beer->alcohol }}</td>
-						<td>$ {{ $beer->price }}</td>
-						<td>$ {{ $beer->happyhour_price }}</td>
-						<td>
-							<button class="btn" data-toggle="modal" data-target="#updateBeerModal-{{ $beer->id }}"><i class="fas fa-pen"></i></button>
-							<!-- I need one modal per beer -->
-							<!--Modal Edit Beer -->
-							@include('admin.beers.edit')
-						</td>
-						<td>
-							{!! Form::open([
-								'route' => ['admin.bars.beers.destroy', $beer->id, $beer->bar->id],
-								'method' => 'DELETE'
-							]) !!}
-								<button type="submit" class="btn"><i class="fas fa-trash"></i></button>
-							{!! Form::close() !!}
-						</td>
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</div>
+	<div class="table-responsive text-nowrap">
+		<table class="table table-striped table-hover w-auto">
+			<thead>
+				<th>Nombre</th>
+				<th>Estilo</th>
+				<th>Descripcion</th>
+				<th>Productor</th>
+				<th>Color</th>
+				<th>IBU</th>
+				<th>Alcohol</th>
+				<th>Precio</th>
+				<th>Precio HH</th>
+				<th>Editar</th>
+				<th>Eliminar</th>
+			</thead>
+			<tbody>
+				@foreach($beers as $beer)
+				<tr>
+					<td>{{ $beer->name }}</td>
+					<td>{{ $beer->style }}</td>
+					<td>{{ $beer->description }}</td>
+					<td>{{ $beer->brand }}</td>
+					<td>{{ $beer->color }}</td>
+					<td>{{ $beer->ibu }}</td>
+					<td>{{ $beer->alcohol }}</td>
+					<td>$ {{ $beer->price }}</td>
+					<td>$ {{ $beer->happyhour_price }}</td>
+					<td>
+						<button class="btn" data-toggle="modal" data-target="#updateBeerModal-{{ $beer->id }}"><i class="fas fa-pen"></i></button>
+						<!-- I need one modal per beer -->
+						<!--Modal Edit Beer -->
+						@include('admin.beers.edit')
+					</td>
+					<td>
+						{!! Form::open([
+							'route' => ['admin.bars.beers.destroy', $beer->id, $beer->bar->id],
+							'method' => 'DELETE'
+						]) !!}
+							<button type="submit" class="btn"><i class="fas fa-trash"></i></button>
+						{!! Form::close() !!}
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
 	</div>
 
 	<div class="d-flex flex-row-reverse" data-toggle="modal" data-target="#newBeerModal">
--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -3,54 +3,52 @@
 @section('content')
 	<h3>Pizzaron de cervezas</h3>
 	
-	<div class="card">
-		<div class="card-body bg-light">
-			<table class="table table-striped table-hover">
-				<thead>
-					<th>Nombre</th>
-					<th>Estilo</th>
-					<th>Descripcion</th>
-					<th>Productor</th>
-					<th>Color</th>
-					<th>IBU</th>
-					<th>Alcohol</th>
-					<th>Precio</th>
-					<th>Precio HH</th>
-					<th>Editar</th>
-					<th>Eliminar</th>
-				</thead>
-				<tbody>
-					@foreach($beers as $beer)
-					<tr>
-						<td>{{ $beer->name }}</td>
-						<td>{{ $beer->style }}</td>
-						<td>{{ $beer->description }}</td>
-						<td>{{ $beer->brand }}</td>
-						<td>{{ $beer->color }}</td>
-						<td>{{ $beer->ibu }}</td>
-						<td>{{ $beer->alcohol }}</td>
-						<td>$ {{ $beer->price }}</td>
-						<td>$ {{ $beer->happyhour_price }}</td>
-						<td>
-							<button class="btn" data-toggle="modal" data-target="#updateBeerModal-{{ $beer->id }}"><i class="fas fa-pen"></i></button>
-							@include('beers.edit')
-						</td>
-						<td>
-							{!! Form::open([
-								'route' => ['beers.destroy', $beer->id],
-								'method' => 'DELETE'
-							]) !!}
-								<button type="submit" class="btn"><i class="fas fa-trash"></i></button>
-							{!! Form::close() !!}
-						</td>
-					</tr>
+	<div class="table-responsive text-nowrap">
+		<table class="table table-striped table-hover w-auto">
+			<thead>
+				<th>Nombre</th>
+				<th>Estilo</th>
+				<th>Descripcion</th>
+				<th>Productor</th>
+				<th>Color</th>
+				<th>IBU</th>
+				<th>Alcohol</th>
+				<th>Precio</th>
+				<th>Precio HH</th>
+				<th>Editar</th>
+				<th>Eliminar</th>
+			</thead>
+			<tbody>
+				@foreach($beers as $beer)
+				<tr>
+					<td class="text-primary"><b>{{ $beer->name }}</b></td>
+					<td>{{ $beer->style }}</td>
+					<td>{{ $beer->description }}</td>
+					<td>{{ $beer->brand }}</td>
+					<td>{{ $beer->color }}</td>
+					<td>{{ $beer->ibu }}</td>
+					<td>{{ $beer->alcohol }}</td>
+					<td>$ {{ $beer->price }}</td>
+					<td>$ {{ $beer->happyhour_price }}</td>
+					<td>
+						<button class="btn" data-toggle="modal" data-target="#updateBeerModal-{{ $beer->id }}"><i class="fas fa-pen"></i></button>
+						@include('beers.edit')
+					</td>
+					<td>
+						{!! Form::open([
+							'route' => ['beers.destroy', $beer->id],
+							'method' => 'DELETE'
+						]) !!}
+							<button type="submit" class="btn"><i class="fas fa-trash"></i></button>
+						{!! Form::close() !!}
+					</td>
+				</tr>
 
-					<!-- I need one modal per beer -->
-					<!--Modal Edit Beer -->
-					@endforeach
-				</tbody>
-			</table>
-		</div>
+				<!-- I need one modal per beer -->
+				<!--Modal Edit Beer -->
+				@endforeach
+			</tbody>
+		</table>
 	</div>
 
 	<div class="d-flex flex-row-reverse" data-toggle="modal" data-target="#newBeerModal">
